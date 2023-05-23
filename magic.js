d2c0b573156e4bfe0c28fa02bc59a693773190c0
Mauricio <79538739+Vulp0@users.noreply.github.com>
2023-05-23 13:41:58 -0600

    results are now an array (easier to read)

--- magic.js
+++ magic.js
@@ -1,9 +1,6 @@
-//Testing ground, seeing how am i gonna handle json files, searching them and comparing them
-//Escape char: \
-// let realFile; //this is a testing file, a placeholder if you will
-
 let reader = new FileReader();
 let cats, biomes, foods, mobs, animals, nether;
+let r_cats = [], r_biomes = [], r_foods = [], r_mobs = [], r_animals = [], r_nether = [];
 let userAdvancementFile;
 
 let fileInput = document.querySelector("#fileInput");
@@ -58,75 +55,94 @@ function handleUserFile(){
 }
 
 function checkAdvancements(){
-    console.log("--Nether biomes left to visit--");
+    //all this is placeholder btw
+    // console.log("--Nether biomes left to visit--");
     if("minecraft:nether/explore_nether" in userAdvancementFile){
         nether.forEach(element => {
             if(! (("minecraft:" + element) in userAdvancementFile["minecraft:nether/explore_nether"]["criteria"])){
-                console.log(element + " not yet visited")
+                // console.log(element + " not yet visited")
+                r_nether.push(element);
             }
         });
     } else {
-        console.log("You haven't made any progress in this advancement yet")
+        r_nether.push("You haven't made any progress in this advancement yet")
     }
 
 
-    console.log("--Hostile mobs left to kill--");
+    // console.log("--Hostile mobs left to kill--");
     if("minecraft:adventure/kill_all_mobs" in userAdvancementFile){
         mobs.forEach(element => {
             if(! (("minecraft:" + element) in userAdvancementFile["minecraft:adventure/kill_all_mobs"]["criteria"])){
-                console.log(element + " not yet killed")
+                // console.log(element + " not yet killed")
+                r_mobs.push(element);
             }
         });
     } else {
-        console.log("You haven't made any progress in this advancement yet")
+        r_mobs.push("You haven't made any progress in this advancement yet")
     }
 
 
-    console.log("--Foods left to eat--");
+    // console.log("--Foods left to eat--");
     if("minecraft:husbandry/balanced_diet" in userAdvancementFile){
         foods.forEach(element => {
             if(! (element in userAdvancementFile["minecraft:husbandry/balanced_diet"]["criteria"])){
-                console.log(element + " not yet eaten")
+                // console.log(element + " not yet eaten")
+                r_foods.push(element);
             }
         });
     } else {
-        console.log("You haven't made any progress in this advancement yet")
+        r_foods.push("You haven't made any progress in this advancement yet")
     }
 
 
-    console.log("--Cats left to tame--");
+    // console.log("--Cats left to tame--");
     if("minecraft:husbandry/complete_catalogue" in userAdvancementFile){
         cats.forEach(element => {
             if(! (("minecraft:" + element) in userAdvancementFile["minecraft:husbandry/complete_catalogue"]["criteria"])){
-                console.log(element + " not yet tamed")
+                // console.log(element + " not yet tamed")
+                r_cats.push(element);
             }
         });
     } else {
-        console.log("You haven't made any progress in this advancement yet")
+        r_cats.push("You haven't made any progress in this advancement yet")
     }
 
 
-    console.log("--Biomes left to explore--");
+    // console.log("--Biomes left to explore--");
     if("minecraft:adventure/adventuring_time" in userAdvancementFile){
         biomes.forEach(element => {
             if(! (("minecraft:" + element) in userAdvancementFile["minecraft:adventure/adventuring_time"]["criteria"])){
-                console.log(element + " not yet visited")
+                // console.log(element + " not yet visited")
+                r_biomes.push(element);
             }
         });
     } else {
-        console.log("You haven't made any progress in this advancement yet")
+        r_biomes.push("You haven't made any progress in this advancement yet")
     }
 
 
-    console.log("--Animals left to breed--");
+    // console.log("--Animals left to breed--");
     if("minecraft:husbandry/bred_all_animals" in userAdvancementFile){
         animals.forEach(element => {
             if(! (("minecraft:" + element) in userAdvancementFile["minecraft:husbandry/bred_all_animals"]["criteria"])){
-                console.log(element + " not bred yet")
+                // console.log(element + " not bred yet")
+                r_animals.push(element);
             }
         });
     } else {
-        console.log("You haven't made any progress in this advancement yet")
+        r_animals.push("You haven't made any progress in this advancement yet")
     }
-
+    //so yucky
+    console.log("Nether biomes remaining:");
+    console.log(r_nether);
+    console.log("Mobs remaining:");
+    console.log(r_mobs);
+    console.log("Foods remaining:");
+    console.log(r_foods);
+    console.log("Cats remaining:");
+    console.log(r_cats);
+    console.log("Biomes remaining:");
+    console.log(r_biomes);
+    console.log("Animals remaining:");
+    console.log(r_animals);
 }
